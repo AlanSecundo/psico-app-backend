58f3a8fbc1cacf64f336e708dbce2c3d920432e9
Alan Secundo <alan_secundo@hotmail.com>
2025-06-15 10:55:44 -0300

    feat: adding Keycloak configuration and security config

--- src/main/java/com/psicoclinic/psicoapp/PsicoappApplication.java
+++ src/main/java/com/psicoclinic/psicoapp/PsicoappApplication.java
@@ -1,4 +1,4 @@
-package com.example.psicoclinic.psicoapp;
+package com.psicoclinic.psicoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/psicoclinic/psicoapp/PsicoappApplicationTests.java
+++ src/test/java/com/psicoclinic/psicoapp/PsicoappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.psicoclinic.psicoapp;
+package com.psicoclinic.psicoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
